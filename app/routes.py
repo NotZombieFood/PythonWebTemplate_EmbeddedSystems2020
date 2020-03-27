9bcbe803850a1bc0e59b80a157b0d7fa9fa1067f
Gerardo Cruz <gerardocd1000@gmail.com>
2020-03-27 08:35:14 -0600

    Added trace command and basic uart commands

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from app import app
 from flask import render_template, redirect, url_for, request, send_from_directory, request
-import os, pickle, time, datetime, serial
+import os, pickle, time, datetime, serial, threading
 
 ########################## GLOBAL VARIABLES, MODIFY AS NECESSARY ###########################################
 COM_PORT = "COM10"
+READ_DATA_DELAY = 5 #This is on seconds
+
+trace = {"val":[], "ts":[]}
 
 #########################  SERIAL COMMUNICATION ######################################
 
@@ -38,6 +41,33 @@ def serialTest():
 
 ####################### BASE ROUTES & FUNCTIONS (DONT MODIFY) ##########################################################
 
+
+# Threaded function for getting values
+def f(f_stop):
+    read_val = uartReceiveWaitUntilVal()
+    timestamp = datetime.datetime.now().strftime('%H:%M:%S')
+    try:
+        trace["val"].append(int(read_val))  # Assuming the value is an integer 
+        trace["ts"].append(timestamp)
+    except:
+        print("Dropped packet as it was not an integer. Packet: %s" % read_val)
+    if not f_stop.is_set():
+        # call f() again in READ_DATA_DELAY seconds
+        threading.Timer(READ_DATA_DELAY, f, [f_stop]).start()
+
+f_stop = threading.Event()
+
+@app.route('/trace')
+def startTracing():
+    """
+        Starting the trace at time 0, hangs Flask. WA is starting the trace manually with a get request to the /trace url
+    """
+    if len(trace["val"]):
+        return "Trace command was already executed"
+    else:
+        f(f_stop)
+        return "Trace command was executed"
+
 @app.route('/test')
 def test():
     """
@@ -55,12 +85,6 @@ def send_static_files(path):
     """
     return send_from_directory('static', path)
 
-
-
-#return render_template('grapher.html', list = trace["val"], time_list = str(trace["ts"]))
-
-
-
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
@@ -68,4 +92,37 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
+
+######################################### USER DEFINED FUNCTIONS ########################################
+def lowerTemp() -> None:
+    uartSend("L")
+
+def higgerTemp() -> None:
+    uartSend("H")
+
+def toggleFan(on=True) -> None:
+    message = "F=T" if on else "F=F" 
+    uartSend(message)
+
+def dimmer(percentage: int) -> None:
+    if percentage <= 100 and percentage >= 0:
+        uartSend("D=%i" % percentage)
+    else:
+        raise Exception("Percentage for dimmer is out of the specified range. Value: %i" % percentage)
+
+######################################## TEMPLATES #####################################################
+
+
+
+
+
+
+
+
+
+
+
+#return render_template('grapher.html', list = trace["val"], time_list = str(trace["ts"]))
+
+
 

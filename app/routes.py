5337c22ff4120db6b86fe47a775ef45b2c20c6af
Gerardo Cruz <gerardocd1000@gmail.com>
2020-03-28 11:57:41 -0600

    added template for dimmer, fan and light control

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from app import app
-from flask import render_template, redirect, url_for, request, send_from_directory, request
+from flask import render_template, redirect, url_for, request, send_from_directory, request, jsonify
 import os, pickle, time, datetime, serial, threading
 
 ########################## GLOBAL VARIABLES, MODIFY AS NECESSARY ###########################################
@@ -37,8 +37,6 @@ def serialTest():
             uartSend(test_object + "\r\n")
     uartSend("Test has finished \r\n")
 
-
-
 ####################### BASE ROUTES & FUNCTIONS (DONT MODIFY) ##########################################################
 
 
@@ -59,9 +57,7 @@ f_stop = threading.Event()
 
 @app.route('/trace')
 def startTracing():
-    """
-        Starting the trace at time 0, hangs Flask. WA is starting the trace manually with a get request to the /trace url
-    """
+    """ Starting the trace at time 0, hangs Flask. WA is starting the trace manually with a get request to the /trace url """
     if len(trace["val"]):
         return "Trace command was already executed"
     else:
@@ -70,19 +66,23 @@ def startTracing():
 
 @app.route('/test')
 def test():
-    """
-        Route: /test
-        This is just for checking if your web server is wroking correctly 
-    """
+    """ This is just for checking if your web server is wroking correctly, look for the easter egg. """
     serialTest()
     return "Test has been executed, check your UART"
 
+
+
+@app.route('/API/serial_command', methods=['POST'])
+def serial_sent_command():
+    """ This is for sending serial commands using the API """
+    req_data = request.form
+    message = req_data['message']
+    uartSend(message)
+    return "Message has been sent"
+
 @app.route('/static/<path:path>')
 def send_static_files(path):
-    """
-        Route: /static/
-        This is used for delivering files to the users, can be uses for css, js, images, etc.
-    """
+    """ This is used for delivering files to the users, can be uses for css, js, images, etc. """
     return send_from_directory('static', path)
 
 @app.errorhandler(404)
@@ -94,6 +94,7 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 ######################################### USER DEFINED FUNCTIONS ########################################
+
 def lowerTemp() -> None:
     uartSend("L")
 
@@ -104,21 +105,61 @@ def toggleFan(on=True) -> None:
     message = "F=T" if on else "F=F" 
     uartSend(message)
 
+def toggleLight(on=True) -> None:
+    message = "L=T" if on else "L=F" 
+    uartSend(message)
+
 def dimmer(percentage: int) -> None:
     if percentage <= 100 and percentage >= 0:
         uartSend("D=%i" % percentage)
     else:
         raise Exception("Percentage for dimmer is out of the specified range. Value: %i" % percentage)
 
-######################################## TEMPLATES #####################################################
+######################################## web views #####################################################
+
+@app.route('/')
+def home():
+    """Print available functions."""
+    func_list = {}
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint != 'static':
+            func_list[rule.rule] = app.view_functions[rule.endpoint].__doc__
+    return jsonify(func_list)
+
+@app.route('/demos/toggle')
+def demo_toggle():
+    """ Run the demo about fan control, light control and dimmer """ 
+    return render_template('toggle.html', command_light = "/API/ToggleLight", command_fan = "/API/ToggleFan", command_dimmer = '/API/Dimmer')
 
 
 
+######################################## API endpoints ########################################
+
+@app.route('/API/ToggleLight', methods=['POST'])
+def toggleLight_endpoint():
+    """ Toggle light, receives a json with status key, which if it is 1 will turn on the light. 0 or anything else turns off the light""""
+    req_data = request.form # using form as we might want to add support for extra lights in the future
+    status = True if req_data['status'] == '1' else False
+    toggleLight(status)
+    return "Light has been succesfully toggled", 200
 
 
+@app.route('/API/Dimmer', methods=['POST'])
+def dimmer_endpoint():
+    """ Dimmer endpoint receives a json file with the value key which can be any value between 0 to 100"""
+    req_data = request.form # using form as we might want to add support for extra dimmer in the future
+    value = int(req_data['value'])
+    dimmer(value)
+    return "Dimmer request has been sent", 200
 
 
-
+@app.route('/API/ToggleFan', methods=['POST'])
+def toggleFan_endpoint():
+    """ Toggle fan, receives a json with status key, which if it is 1 will turn on the fan. 0 or anything else turns off the fan""""
+    req_data = request.form # using form as we might want to add support for extra fans in the future
+    status = True if req_data['status'] == '1' else False
+    toggleFan(status)
+    return "Fan has been succesfully toggled", 200
 
 
 
